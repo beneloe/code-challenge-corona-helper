c2235b6c0c8cad8049f05c618dbddd6212015c9a
Benedikt Loeffler <benedikt.loe@hotmail.com>
2021-06-26 16:13:10 +0200

    Add basic scraping functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
+require 'open-uri'
+require 'nokogiri'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    html_content = URI.open('https://www.doctolib.de/kinderheilkunde-kinder-und-jugendmedizin/berlin').read
+    @doc = Nokogiri::HTML(html_content)
   end
 end
