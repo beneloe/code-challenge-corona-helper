71ac9f1e25ea97ea076b7dc730f631d8e867ff00
Benedikt Loeffler <benedikt.loe@hotmail.com>
2021-06-26 17:45:22 +0200

    Add proper css scraping selectors for physicians

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,27 @@ require 'nokogiri'
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
+  class Physician
+    attr_reader :name, :specialty, :address
+
+    def initialize(name, specialty, address)
+      @name = name
+      @specialty = specialty
+      @address = address
+    end
+  end
+
   def home
     html_content = URI.open('https://www.doctolib.de/kinderheilkunde-kinder-und-jugendmedizin/berlin').read
-    @doc = Nokogiri::HTML(html_content)
+    doc = Nokogiri::HTML(html_content)
+
+    entries = doc.css('.dl-search-result-presentation')
+    @entries_array = []
+    entries.each do |entry|
+      name = entry.css('h3 a div').text
+      specialty = entry.css('.dl-search-result-subtitle').text
+      address = entry.css('.dl-text.dl-text-body.dl-text-s.dl-text-regular>div').text
+      @entries_array << Physician.new(name, specialty, address)
+    end
   end
 end
