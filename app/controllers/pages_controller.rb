c691106b6f5db831a2bc73811bb7535b8bd1a54d
Benedikt Loeffler <benedikt.loe@hotmail.com>
2021-06-27 16:17:16 +0200

    Add counsellors to search and listing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,29 +5,59 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   class Physician
-    attr_reader :name, :specialty, :address
+    attr_reader :name, :specialty, :address, :number
 
-    def initialize(name, specialty, address)
+    def initialize(name, specialty, address, number)
       @name = name
       @specialty = specialty
       @address = address
+      @number = number
+    end
+  end
+
+  class Counsellor
+    attr_reader :name, :specialty, :address, :number
+
+    def initialize(name, specialty, address, number)
+      @name = name
+      @specialty = specialty
+      @address = address
+      @number = number
     end
   end
 
   def home
     @search = search_params
     if @search.present?
-      @address = @search["address"].downcase.split(",")[0]
+      @address = @search["address"].split(",")[0]
       unless @address.empty?
-        html_content = URI.open("https://www.doctolib.de/kinderheilkunde-kinder-und-jugendmedizin/#{@address}").read
-        doc = Nokogiri::HTML(html_content)
-        entries = doc.css('.dl-search-result-presentation')
-        @entries_array = []
-        entries.each do |entry|
-          name = entry.css('h3 a div').text
-          specialty = entry.css('.dl-search-result-subtitle').text
-          address = entry.css('.dl-text.dl-text-body.dl-text-s.dl-text-regular>div').text
-          @entries_array << Physician.new(name, specialty, address)
+        html_content_physicians = URI.open("https://www.gelbeseiten.de/Suche/kinderarzt/#{@address}?umkreis=20000").read
+        doc_physicians = Nokogiri::HTML(html_content_physicians)
+        physicians = doc_physicians.css('article.mod.mod-Treffer')
+        @physicians_array = []
+        physicians.each do |physician|
+          name = physician.css('a h2').text
+          physician.css('p.d-inline-block.mod-Treffer--besteBranche').text.include?("Jugendmedizin") ? specialty = "Pediatrician" : nil
+          address = physician.css('address.mod.mod-AdresseKompakt>p').first.text
+          number = physician.css('p.mod-AdresseKompakt__phoneNumber').text
+          @physicians_array << Physician.new(name, specialty, address, number)
+        end
+      end
+    end
+
+    if @search.present?
+      @address = @search["address"].split(",")[0]
+      unless @address.empty?
+        html_content_counsellors = URI.open("https://www.gelbeseiten.de/Suche/Jugendaemter/#{@address}?umkreis=20000").read
+        doc_counsellors = Nokogiri::HTML(html_content_counsellors)
+        counsellors = doc_counsellors.css('article.mod.mod-Treffer')
+        @counsellors_array = []
+        counsellors.each do |counsellor|
+          name = counsellor.css('a h2').text
+          counsellor.css('p.d-inline-block.mod-Treffer--besteBranche').text.include?("Jugend") ? specialty = "Youth Counsellor" : nil
+          address = counsellor.css('address.mod.mod-AdresseKompakt>p').first.text
+          number = counsellor.css('p.mod-AdresseKompakt__phoneNumber').text
+          @counsellors_array << Counsellor.new(name, specialty, address, number)
         end
       end
     end
