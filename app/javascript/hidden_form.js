00ecf7485ee1dab4d8edb0e8080569d8567f2ca1
Benedikt Loeffler <benedikt.loe@hotmail.com>
2021-07-02 16:38:40 +0200

    Delete hidden form

--- app/javascript/hidden_form.js
+++
@@ -1,7 +0,0 @@
-const hiddenForm = () => {
-  var button = document.getElementById('search-btn')
-  var hidden_form = document.getElementById('hidden-form')
-  button.onsubmit = hidden_form.submit;
-};
-
-hiddenForm();
